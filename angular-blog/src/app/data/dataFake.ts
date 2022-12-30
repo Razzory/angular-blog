09d53113227f0c19d8db740fe4cff8fe2954d5db
Gustavo <gustavo.okagawa@gmail.com>
2022-12-30 11:01:41 -0300

    content created

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Homem Aranha 1",
+    "description": "foda-se",
+    "photoCover": "https://lumiere-a.akamaihd.net/v1/images/iron-man_dft_m_db79b94b.jpeg"
+  },
+
+  {
+    "id": "2",
+    "title": "Homem Aranha 2",
+    "description": "foda-se também",
+    "photoCover": ""
+  }
+]
+
