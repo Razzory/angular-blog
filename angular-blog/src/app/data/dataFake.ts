35328fa4d0097755a56d64fe5bc1b06c69ca29f6
Gustavo <gustavo.okagawa@gmail.com>
2022-12-30 21:20:53 -0300

    arquivos finalizados para entrega de projeto

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,16 +1,31 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "Homem Aranha 1",
-    "description": "foda-se",
-    "photoCover": "https://lumiere-a.akamaihd.net/v1/images/iron-man_dft_m_db79b94b.jpeg"
+    "title": "Warbug Pincus",
+    "description": "Bootcamp Warburg Pincus da DIO. Trata-se de um bootcamp de 151 horas de conteúdo onde serão abordados JavaScript, Java, Angular, SQL, NoSQL, Spring Framework e Clouse Computer AWS",
+    "photoCover": "https://hermes.digitalinnovation.one/tracks/d2c90131-df30-4a38-bddc-f8280f19cbf9.png"
   },
 
   {
     "id": "2",
-    "title": "Homem Aranha 2",
-    "description": "foda-se também",
-    "photoCover": ""
+    "title": "Certificados de Angular",
+    "description": "Este blog é parte do projeto de conclusão do curso de Angular do Bootcamp Warburg Pincus da DIO",
+    "photoCover": "https://hermes.digitalinnovation.one/tracks/5a199bba-a494-49ce-b357-f746eb7b7425.png"
+  },
+
+
+  {
+    "id": "3",
+    "title": "Certificados de Javascpript",
+    "description": "Curso de JavaScript e TypeScript do Bootcamp WArburg Pincus da DIO finalizados.",
+    "photoCover": "https://hermes.digitalinnovation.one/tracks/55e7040f-775b-47e5-a8fb-69d002ca17a9.png"
+  },
+
+  {
+    "id": "4",
+    "title": "Certificados de HTML",
+    "description": "Curso de HTMLS e CSS do Bootcamp WArburg Pincus da DIO finalizados.",
+    "photoCover": "https://hermes.digitalinnovation.one/tracks/62ed1f1d-8d76-4bbc-905f-e73d20cb82f5.png"
   }
 ]
 
